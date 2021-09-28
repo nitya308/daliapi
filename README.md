6b06a26925b2c6ed3e32b0961110298b18eb3b16
Tyler Vergho <tvergho@gmail.com>
2021-09-28 17:16:04 -0400

    moved to yarn, updated README

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# starter express app template
+# Starter Express App Template
+
+## Overview 
 
 * node with babel
 * expressjs
 * airbnb eslint rules
 
-Procfile set up to run on [heroku](https://devcenter.heroku.com/articles/getting-started-with-nodejs#deploy-the-app)
+Procfile set up to run on [heroku](https://devcenter.heroku.com/articles/getting-started-with-nodejs#deploy-the-app).
+
+Credit to Tim Tregubov's [CS52 course](https://cs52.me/assignments/) for providing the basic code in this starter pack.
+
+## Backend Mini-Series Project Description
--- src/server.js
+++ src/server.js
@@ -12,15 +12,6 @@ app.use(cors());
 // enable/disable http request logging
 app.use(morgan('dev'));
 
-// enable only if you want templating
-app.set('view engine', 'ejs');
-
-// enable only if you want static assets from folder static
-app.use(express.static('static'));
-
-// this just allows us to render ejs from the ../app/views directory
-app.set('views', path.join(__dirname, '../src/views'));
-
 // enable json message body for posting data to API
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // To parse the incoming requests with JSON payloads
