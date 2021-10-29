bade325fc946a978cf65fae8b597f9c8e7c160aa
nitya308 <64368452+nitya308@users.noreply.github.com>
2021-10-28 22:52:33 -0400

    fixed referencing to Posts

--- src/models/comment.js
+++ src/models/comment.js
@@ -3,7 +3,7 @@ import mongoose, { Schema } from 'mongoose';
 /* This model stores all the comments */
 
 const CommentSchema = new Schema({
-  post: [{ type: Schema.Types.ObjectId, ref: 'Post' }], // Each comment is made to one post (comments--> posts = many --> one)
+  post: [{ type: Schema.Types.ObjectId, ref: 'Posts' }], // Each comment is made to one post (comments--> posts = many --> one)
   commentor: [{ type: Schema.Types.ObjectId, ref: 'User' }], // Each comment is made by one user (comments--> users = many --> one)
   text: String,
 });
